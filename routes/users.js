b6cfd06de691a66a26e95a7fab5203db61261661
Dino Febriyanto <dino.febrianto@zettabyte.life>
2022-05-18 10:35:38 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ router.post('/', function (req, res, next) {
   const result = req.body;
   //Get Intent
   const intent = result.queryResult.intent.displayName;
+  const language = result.queryResult.languageCode;
   const query = result.queryResult.queryText;
   const responds = result.queryResult.fulfillmentMessages;
 
@@ -21,16 +22,18 @@ router.post('/', function (req, res, next) {
     const magnitude = result.queryResult.sentimentAnalysisResult.queryTextSentiment.magnitude;
 
 
-
-
-
     // store the result to DB
     SentimentAnalysisModel.create({
       score, magnitude, query, responds, intent
     })
 
     if (score < -0.85) {
-      res.send(createTextResponse("Sorry if my perfomance is bad :( If there is Information that i can't answer, you can contact my human friends through Contact Us Feature :)"));
+      if (language == "en"){
+        res.send(createTextResponse("Sorry if my perfomance is bad :( If there is Information that i can't answer, you can contact my human friends through Contact Us Feature :)"));
+      } else {
+        res.send(createTextResponse("Désolé si ma performance est mauvaise :( S'il y a des informations auxquelles je ne peux pas répondre, vous pouvez contacter mes amis humains via la fonction Contactez-nous :)"));
+      }
+      
     }
 
   } else {

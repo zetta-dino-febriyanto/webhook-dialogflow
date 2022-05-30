dd3418bc36766ac2d5a6a2f65e3b6244d112692b
Dino Febriyanto <dino.febrianto@zettabyte.life>
2022-05-30 13:08:39 +0700

    chage router name for v1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var v1Router = require('./routes/users');
 var v2Router = require('./routes/v2_new');
 var userRouter = require('./routes/bilip_user');
 var app = express();
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', v1Router);
 app.use('/v2_new', v2Router);
 app.use('/bilip_user', userRouter);
 

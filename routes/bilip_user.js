95ee00177a6aca81716742d9f2fb904ff4332f36
Dino Febriyanto <dino.febrianto@zettabyte.life>
2022-06-16 08:49:43 +0700

    Update bilip_user.js

--- routes/bilip_user.js
+++ routes/bilip_user.js
@@ -178,6 +178,7 @@ const dialogflowfulfillment = (request, response, result) => {
     const id_before = result.originalDetectIntentRequest.payload.userId;
     const results = id_before.split(/[/\s]/);
     const id = results[0];
+    console.log(id)
 
     // function to send email to user help: Dear User Help. Our customer with name <<customer name>> and email <<customer email>> have problem. Please contact them. Thank you.
     let student = await common.get_data(

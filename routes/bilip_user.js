b0802f2d2eb1f8159726058665dd2ead04f261c7
Dino Febriyanto <dino.febrianto@zettabyte.life>
2022-06-23 14:00:26 +0700

    Update bilip_user.js

--- routes/bilip_user.js
+++ routes/bilip_user.js
@@ -120,6 +120,7 @@ const dialogflowfulfillment = (request, response, result) => {
     const language_code = result.queryResult.languageCode;
     //get user data
     //uncommend if on stagging
+    console.log(id_before)
     const id_before = result.originalDetectIntentRequest.payload.userId;
     const results = id_before.split(/[/\s]/);
     const id = results[0];
